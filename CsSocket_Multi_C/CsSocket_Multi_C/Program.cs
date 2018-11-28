9a4a611e60a5617e7cc40c66d1ec0e2943eaed15
harryuan65 <harryuan.65@gmail.com>
2018-11-28 17:59:39 +0800

    add files

---
+++ CsSocket_Multi_C/CsSocket_Multi_C/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CsSocket_Multi_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect("127.0.0.1",8);
+            s.Close();
+            s.Dispose();
+
+        }
+    }
+}
